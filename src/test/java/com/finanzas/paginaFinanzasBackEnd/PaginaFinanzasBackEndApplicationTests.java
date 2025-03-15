6f3dfefe7011fe6e4dc29a895b200d2328a94a3b
Tomas Colombo <tomicolombo20051@gmail.com>
2025-03-15 09:58:11 -0300

    Init

---
+++ src/test/java/com/finanzas/paginaFinanzasBackEnd/PaginaFinanzasBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzas.paginaFinanzasBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaFinanzasBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
